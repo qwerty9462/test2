1bda4762632b59453e8ca7ae3ccf51ea81f4297f
qwerty9462 <qwerty9462@gmail.com>
2021-08-29 20:34:35 +0800

    60persentageComplete

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class LoadAssets : MonoBehaviour
 {
+    public GameObject redObj;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Test");
+        
     }
 
     // Update is called once per frame
